5874dab5ffccd7709a74e5e8eb06df19b8348800
laiagc892 <laiagc892@gmail.com>
2021-02-26 12:57:55 +0100

    car movement & event style

--- js/gameFunctions.js
+++ js/gameFunctions.js
@@ -3,14 +3,22 @@
 - Funció recursiva que si no es clica pausa, crida el setTimeOut() perquè segueixi movent-se (cridant la funció).
 */
 
-setTimeout(landscapeMovement, 10);
-
+// Landscape movement:
 var landscape = document.querySelector("#landscape");
 var position = 0;
+// Game timing:
 var pauseButton = document.querySelector("#pause");
 var pause = false;
 var gameTime = 0;
 var gameEnd = false;
+// Car movement:
+var carBody = document.querySelector("#car-body");
+var carWheelFront = document.querySelector("#car-wheel-front");
+var carWheelBack = document.querySelector("#car-wheel-back");
+var rotation = 0;
+var bounce = 0;
+
+setTimeout(landscapeMovement, 10);
 
 pauseButton.addEventListener("click", () => {
     if (pause) {
@@ -39,37 +47,49 @@ function landscapeMovement() {
     landscape.style.transform = `translateX(${position}px)`;
     
     if (gameTime == 16000) {
-        // landscape.style.transform = `translateX(0px)`;
         position = 0;
+        console.log(position);
         alert("event1");
         landscape.style.backgroundColor = "grey";
         landscape.style.backgroundImage = "url('./img/bg_test/testbg-first.png')";
     }
     if (gameTime == 32000) {
-        // landscape.style.transform = `translateX(0px)`;
         position = 0;
         alert("event2");
         landscape.style.backgroundColor = "gray";
         landscape.style.backgroundImage = "url('./img/bg_test/testbg-first.png')";
     }
     if (gameTime == 48000) {
-        // landscape.style.transform = `translateX(0px)`;
         position = 0;
         alert("event3");
         landscape.style.backgroundColor = "lightgrey";
         landscape.style.backgroundImage = "url('./img/bg_test/testbg-first.png')";
     }
     if (gameTime == 64000) {
-        // landscape.style.transform = `translateX(0px)`;
         position = 0;
         alert("event4");
         landscape.style.backgroundColor = "white";
         landscape.style.backgroundImage = "url('./img/bg_test/testbg-first.png')";
         gameEnd = true;
     }
-
     
     if (!pause && !gameEnd) {
         setTimeout(landscapeMovement, 10);
+        setTimeout(carMovement, 10);
+    }
+}
+
+function carMovement() {
+    rotation += 5;
+    carWheelFront.style.transform = `rotate(${rotation}deg)`;
+    carWheelBack.style.transform = `rotate(${rotation}deg)`;
+    if (rotation == 360) {
+        rotation = 0;
+    }
+
+    bounce += 0.1;
+    carBody.style.transform = `translateY(${bounce}px)`;
+    if (Math.round(bounce) == 3) {
+        bounce = 0;
     }
 }
