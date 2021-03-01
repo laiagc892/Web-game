5a10034b92c5179866ea46c2dc416225bb227b46
rachurion <rachurion@gmail.com>
2021-03-01 20:48:06 +0100

    R- eventFour first draft

--- js/event-4.js
+++ js/event-4.js
@@ -1,5 +1,25 @@
 // start of functions for event four upgrade
+
 function eventFour() {
-    document.querySelector("#event4").style.visibility = "visible"
+    pauseButton.disabled = "true";
+    document.querySelector("#event4").style.visibility = "visible";
+    let checkButton = document.querySelector("#event4-check");
+    let gameEnd = document.querySelector("#event4-finish");
+    let currentEnergy =  document.querySelector(".current-energy .image-container")
+
+    checkButton.addEventListener("click", () => {
+        if (currentEnergy.innerHTML != "") {
+            let energyImage = currentEnergy.querySelector("img");
+            if (energyImage.id == "event4-electric") {
+                let car = document.querySelector("#event4-car");
+                let wheelFront = car.querySelector("#event4-car-wheel-front");
+                let wheelBack = car.querySelector("#event4-car-wheel-back");
+
+                car.style.transform = "translateX(600px)";
+                wheelFront.style.transform = "rotate(500deg)";
+                wheelBack.style.transform = "rotate(500deg)";
+            }
+        }
+    })
 }
 // end of functions for event four upgrade
