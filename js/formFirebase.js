693415ea82e10f5fb1e670efa6f892009377a0e7
laiagc892 <laiagc892@gmail.com>
2021-03-02 14:58:40 +0100

    send fixed

--- js/formFirebase.js
+++ js/formFirebase.js
@@ -4,7 +4,6 @@ function feedbackForm() {
 
 var sendButton = document.querySelector("#send");
 var errorMessage = document.querySelector("#error");
-var resultsButton = document.querySelector("#get-results");
 var resultsShowed = document.querySelector("#results");
 
 
@@ -13,7 +12,7 @@ const database = "https://ecolucio-joc-default-rtdb.firebaseio.com/";
 
 //* POST ------------------------------------------------------------ ------------------------------------------------------------
 
-sendButton.addEventListener("click", () => {                                        // POST Click
+sendButton.addEventListener("click", () => {                                        // POST & GET Click
     var answer1 = document.querySelector("input[name=question1]:checked");
     var answer2 = document.querySelector("input[name=question2]:checked");
     var answer3 = document.querySelector("input[name=question3]:checked");
@@ -30,6 +29,8 @@ sendButton.addEventListener("click", () => {                                    
     answer2.checked = false;
     answer3.checked = false;
     answer4.value = "";
+
+    getA();
 });
 
 function postA(a1, a2, a3, a4) {                                                    // POST Funció. Posteja el missatge
@@ -51,8 +52,6 @@ function postA(a1, a2, a3, a4) {                                                
 
 
 //* GET ------------------------------------------------------------ ------------------------------------------------------------
-
-resultsButton.addEventListener("click", () => getA())                               // GET Click
 
 function getA() {                                                                   // GET Funció. Carrega tots els resultats actuals
     fetch(database + "form-answers.json")
@@ -87,13 +86,10 @@ function getA() {                                                               
                 feedback.q4.comments.push(results[idR].q4)
             }
         }
+
         console.log(feedback.q1.answers, feedback.q2.answers, feedback.q3.answers, feedback.q4.comments);
-
-
     });
 }
-
-getA()
 
 
 var feedback = new Vue({
@@ -115,5 +111,27 @@ var feedback = new Vue({
             question: "4) Vols afegir algun comentari sobre aquest tema?",
             comments: []
         },
+    },
+    methods: {
+        boto: () => {                                        // POST & GET Click
+            var answer1 = document.querySelector("input[name=question1]:checked");
+            var answer2 = document.querySelector("input[name=question2]:checked");
+            var answer3 = document.querySelector("input[name=question3]:checked");
+            var answer4 = document.querySelector("#a4");
+        
+            if(!answer1 || !answer2 || !answer3) {
+                errorMessage.innerHTML = "Et falta escollir alguna resposta.";
+                return console.log("L'usuari no ha escollit alguna resposta!")              // Si algun camp està buit, no s'envia res i surt de la funció
+            }
+        
+            postA(answer1.value, answer2.value, answer3.value, answer4.value);
+        
+            answer1.checked = false;                                                        // Buida el formulari
+            answer2.checked = false;
+            answer3.checked = false;
+            answer4.value = "";
+        
+            getA();
+        }
     }
 })
